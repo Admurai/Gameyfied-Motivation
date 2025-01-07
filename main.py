53fec76dfbc97112b170e82fe1a3a0397d98bc8b
Admurai <89091358+Admurai@users.noreply.github.com>
2025-01-07 20:46:47 +0100

    Add functionality to manage projects and habits with add/remove options

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ class GUI:
         self.bad_habits = []
         self.good_habits = []
 
+        self.change
         self.show_all_Items
         
     def add_projects (self, job, points):
@@ -59,12 +60,10 @@ class GUI:
 
     def show_projects(self):
         for project in self.projects:
-            print(f"{project.job} - {project.points} - {project.completed}")
-    
+            print(f"{project.job} - {project.points} - {project.completed}")   
     def show_bad_habits(self):
         for bad_habit in self.bad_habits:
-            print(f"{bad_habit.name} - {bad_habit.points} - {bad_habit.completed}")
-    
+            print(f"{bad_habit.name} - {bad_habit.points} - {bad_habit.completed}")    
     def show_good_habits(self):
         for good_habit in self.good_habits:
             print(f"{good_habit.name} - {good_habit.points} - {good_habit.completed}")
@@ -74,19 +73,38 @@ class GUI:
         if option == "y":
             self.complete()
         else:
-            print("Ok")
-
-    def complete(self):
-        option = input("Which Type would u like to complete ? ")
+            option =input ("would u like to add any ? (y/n)")
+            if option == "y":
+                self.add()
+            else:
+                option =input("Would u like to remove any ?(y/n)")
+                if option == "y":
+                    self.remove()
+                else:
+                    option= input("Would u like to see your points ? (y/n)") 
+                    if option == "y":
+                        self.show_points()
+                    else:
+                        option = input("would u like to close the program ? (y/n)")
+                        if option == "y":
+                            print("Goodbye")
+                            #save the data
+                        else:
+                                self.change()
+    
         
-        if option == "project":
+    def complete(self):
+        option= input ("Would U like to add 1/ a project 2/ a bad habit 3/ a good habit ?")
+        
+        if option == "1":
             self.complete_project()
-        elif option == "bad_habit":
+        elif option == "2":
             self.complete_bad_habit()
-        elif option == "good_habit":
+        elif option == "3":
             self.complete_good_habit()
         else:
-            print("Invalid Option")
+            print("please enter a number between 1 and 3")
+            self.complete()
     
     def complete_project(self):
         iteration = 1
@@ -94,23 +112,104 @@ class GUI:
             print(f"{iteration}:  {project.job} - {project.points} - {project.completed}")
             iteration+=1
         option = input("Which Project would u like to complete ?")
+        self.point_count += self.projects[int(option)-1].points
         self.projects[int(option)-1].complete()
         print("Project Completed")
-    
     def complete_bad_habit(self):
         iteration = 1
         for bad_habit in self.bad_habits:
             print(f"{iteration}:  {bad_habit.name} - {bad_habit.points} - {bad_habit.completed}")
             iteration+=1
         option = input("Which Bad Habit would u like to complete ?")
+        self.point_count
         self.bad_habits[int(option)-1].complete()
-        print("Bad Habit failed")
-    
+        print("Bad Habit failed") 
     def complete_good_habit(self):
+
         iteration = 1
         for good_habit in self.good_habits:
             print(f"{iteration}:  {good_habit.name} - {good_habit.points} - {good_habit.completed}")
             iteration+=1
         option = input("Which Good Habit would u like to complete ?")
+        self.point_count += self.good_habits[int(option)-1].points
         self.good_habits[int(option)-1].complete()
         print("Good Habit Completed")
+
+    def add(self):
+        option= input ("Would U like to add 1/ a project 2/ a bad habit 3/ a good habit ?")
+        if option == "1":
+            self.add_project()
+        elif option == "2":
+            self.add_bad_habit()
+        elif option == "3":
+            self.add_good_habit()
+        else:
+            print("please enter a number between 1 and 3")
+            self.add()
+
+    def add_project(self):
+        job = input("Enter the name of the project")
+        points = int(input("How many points is this project worth ?"))
+        self.add_projects(job, points)
+        print("Project Added")
+    def add_bad_habit(self):
+        name = input("Enter the name of the bad habit")
+        points = int(input("How many points is this bad habit worth(negative) ?"))
+        self.add_bad_habits(name, points)
+        print("Bad Habit Added")
+    def add_good_habit(self):
+        name = input("Enter the name of the good habit")
+        points = int(input("How many points is this good habit worth ?"))
+        self.add_good_habits(name, points)
+        print("Good Habit Added")
+    
+    def remove(self):
+        option= input ("Would U like to add 1/ a project 2/ a bad habit 3/ a good habit ?")
+        
+        if option == "1":
+            self.remove_project()
+        elif option == "2":
+            self.remove_bad_habit()
+        elif option == "3":
+            self.remove_good_habit()
+        else:
+            print("please enter a number between 1 and 3")
+            self.remove()
+
+    def remove_project(self):
+        iteration = 1
+        for project in self.projects:
+            print(f"{iteration}:  {project.job} - {project.points} - {project.completed}")
+            iteration+=1
+        option = input("Which Project would u like to remove ?")
+        self.projects.pop(int(option)-1)
+        print("Project Removed")
+    def remove_bad_habit(self):
+        iteration = 1
+        for bad_habit in self.bad_habits:
+            print(f"{iteration}:  {bad_habit.name} - {bad_habit.points} - {bad_habit.completed}")
+            iteration+=1
+        option = input("Which Bad Habit would u like to remove ?")
+        self.bad_habits.pop(int(option)-1)
+        print("Bad Habit Removed")
+    def remove_good_habit(self):
+        iteration = 1
+        for good_habit in self.good_habits:
+            print(f"{iteration}:  {good_habit.name} - {good_habit.points} - {good_habit.completed}")
+            iteration+=1
+        option = input("Which Good Habit would u like to remove ?")
+        self.good_habits.pop(int(option)-1)
+        print("Good Habit Removed")
+
+    def show_points(self):
+        print(f"U have {self.point_count} points")
+    
+
+
+
+app = GUI()
+app.change()
+app.show_all_Items()
+
+
+    
